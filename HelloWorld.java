74fd14350dbeed46473764086132ab815bdc697b
Frans Gomelino <31508325+Frans99@users.noreply.github.com>
2017-09-01 17:44:51 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Nama : Frans Gomelino");
+        System.out.println("NIM : 17220035");
+     }
+}
